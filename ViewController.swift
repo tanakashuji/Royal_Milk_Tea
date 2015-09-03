4b2ad06db00949946f6538a7fdaca8b5fc2cb295
Shuji Tanaka <macgeek@tanakashuji.jp>
2015-09-04 03:29:38 +0900

    1st time
    1st time

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Royal_Milk_Tea
+//
+//  Created by TANAKASHUJI on 2015/09/04.
+//  Copyright © 2015年 TANAKASHUJI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
